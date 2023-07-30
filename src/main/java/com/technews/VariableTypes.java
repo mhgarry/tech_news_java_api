2527800c109d760e845e4bfee91eed909671c507
matt <mhgarry92@gmail.com>
2023-07-29 22:19:02 -0400

    variables

---
+++ src/main/java/com/technews/VariableTypes.java
@@ -0,0 +1,12 @@
+package com.technews;
+
+public class VariableTypes {
+    public static void main(String[] args) {
+        String name = "Cleopatra"; // 'String' indicates the datatype of the variable will be a string
+        int age = 2016 + 69; //'int' indicates the datatype of the variable will be an integer (whole number)
+        double price = 1999.99;  // 'double' indicates that the datatype will be a decmil number
+        boolean truth = true; // 'boolean' indicates that the datype of the variable will be truthy or falsy
+    }
+} //public classes mean code can be used outside of particular class
+ // in the () we define our arguments which in this particular class is an array of Strings
+// Java is staticly typed meaning we need to define variables with their type before declaring them and we cannot change their types
